fe513117907f3a310406ac57382ada0b0118faa7
Nimra <nimramoveed@gmail.com>
2025-01-12 15:53:44 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@ struct movieDetail{
     string name;
     int id;
     int movieSeat;
+    int initialSeats;
+    int bookedSeats;
 };
 movieDetail movies[5];
 
 void initializeMovies() {
-    movies[0] = {1, "Frozen II", 432, 100};
-    movies[1] = {2, "Titanic", 654, 50};
-    movies[2] = {3, "Spider-Man", 639, 50};
-    movies[3] = {4, "Toy Story 4", 598, 50};
-    movies[4] = {5, "The Lion King", 412, 150};
+    movies[0] = {1, "Frozen II", 432, 100, 0};
+    movies[1] = {2, "Titanic", 654, 50, 0};
+    movies[2] = {3, "Spider-Man", 639, 50, 0};
+    movies[3] = {4, "Toy Story 4", 598, 50, 0};
+    movies[4] = {5, "The Lion King", 412, 150, 0};
 }
 
 void viewMovies() {
@@ -23,138 +25,64 @@ void viewMovies() {
         cout << "Movie Name: " << movies[i].name << endl;
         cout << "Id#: " << movies[i].id << endl;
         cout << "Available seats for the movie are: " << movies[i].movieSeat << endl;
+        cout << "Total booked seats for the movie are: " << movies[i].bookedSeats << endl;
     }
 }
-void one(){
-    movieDetail movie1;
-    int tckt;
-    
-    movie1.name="Frozen II";
-    cout<<"The Available seats for  "<<movie1.name;
-    movie1.movieSeat=100;
-    cout<<" are: "<<movie1.movieSeat<<endl;
-    
-    cout<<"enter number of tickets"<<endl;
-    cin>>tckt;
-    
-    if(tckt >movie1.movieSeat){
-        cout<<"please enter valid number of tickets"<<endl;
-    }else{
-        movie1.movieSeat -= tckt;
-        cout<<"tickets booked!! now Available seats are: "<<movie1.movieSeat;
-    }
-    
-}
-void two(){
-    movieDetail movie2;
-    int tckt;
-    
-    movie2.name="Titanic";
-    cout<<"The Available seats for  "<<movie2.name;
-    movie2.movieSeat=50;
-    cout<<" are: "<<movie2.movieSeat<<endl;
-    
-    cout<<"enter number of tickets"<<endl;
-    cin>>tckt;
-    
-    if(tckt >movie2.movieSeat){
-        cout<<"please enter valid number of tickets"<<endl;
-    }else{
-        movie2.movieSeat -= tckt;
-        cout<<"tickets booked!! now Available seats are: "<<movie2.movieSeat;
-    }
-}
-void three(){
-    movieDetail movie3;
-    int tckt;
-    
-    movie3.name="Spider-Man";
-    cout<<"The Available seats for  "<<movie3.name;
-    movie3.movieSeat=50;
-    cout<<" are: "<<movie3.movieSeat<<endl;
-    
-    cout<<"enter number of tickets"<<endl;
-    cin>>tckt;
-    
-    if(tckt >movie3.movieSeat){
-        cout<<"please enter valid number of tickets"<<endl;
-    }else{
-        movie3.movieSeat -= tckt;
-        cout<<"tickets booked!! now Available seats are: "<<movie3.movieSeat;
-    }
 
-}
-void four(){
-    movieDetail movie4;
-    int tckt;
-    
-    movie4.name="Toy Story 4";
-    cout<<"The Available seats for  "<<movie4.name;
-    movie4.movieSeat=50;
-    cout<<" are: "<<movie4.movieSeat<<endl;
-    
-    cout<<"enter number of tickets"<<endl;
-    cin>>tckt;
-    
-    if(tckt >movie4.movieSeat){
-        cout<<"please enter valid number of tickets"<<endl;
-    }else{
-        movie4.movieSeat -= tckt;
-        cout<<"tickets booked!! now Available seats are: "<<movie4.movieSeat;
-    }
-}
-void five(){
-    movieDetail movie5;
-    int tckt;
-    
-    movie5.name="The Lion King";
-    cout<<"The Available seats for   "<<movie5.name;
-    movie5.movieSeat=150;
-    cout<<" are: "<<movie5.movieSeat<<endl;
-    
-    cout<<"enter number of tickets"<<endl;
-    cin>>tckt;
-    
-    if(tckt >movie5.movieSeat){
-        cout<<"please enter valid number of tickets"<<endl;
-    }else{
-        movie5.movieSeat -= tckt;
-        cout<<"tickets booked!! now Available seats are: "<<movie5.movieSeat;
-    }
-}
 void bookTickets(){
-    int slct;
-    cout<<"Select a Movie Number"<<endl;
+    int slct,tckt;
     viewMovies();
+    cout<<"Select a Movie Number"<<endl;
     cin>>slct;
-    switch(slct){
-        case 1:
-        one();
-        break;
-        
-        case 2:
-        two();
-        break;
-        
-        case 3:
-        three();
-        break;
-        
-        case 4:
-        four();
-        break;
-        
-        case 5:
-        five();
-        break;
-        
-        default:
-        cout<<"enter valid movie number"<<endl;
+    
+    if (slct < 1 || slct > 5) {
+        cout << "Please enter a valid movie number." << endl;
+        return;
+    }
+    
+    movieDetail &selectedMovie = movies[slct - 1];
+    
+    cout << "The Available seats for " << selectedMovie.name << " are: " << selectedMovie.movieSeat << endl;
+    cout << "Enter number of tickets: ";
+    cin >> tckt;
+    
+    if (tckt > selectedMovie.movieSeat) {
+        cout << "Please enter a valid number of tickets." << endl;
+    } else {
+        selectedMovie.movieSeat -= tckt;
+        selectedMovie.bookedSeats += tckt;  
+        cout << "Tickets booked! Now available seats for " << selectedMovie.name << " are: " << selectedMovie.movieSeat << endl;
     }
     
 }
 void cancel(){
-    cout<<"Tickets cancelled!!"<<endl;
+    
+    int slct, cancelTickets;
+    cout << "Enter the movie number you want to cancel tickets for: ";
+    cin >> slct;
+
+    if (slct < 1 || slct > 5) {
+        cout << "Invalid movie number!" << endl;
+        return;
+    }
+
+    movieDetail &selectedMovie = movies[slct - 1];
+
+    cout << "You have booked " << selectedMovie.bookedSeats << " tickets for " << selectedMovie.name << endl;
+    cout << "Enter the number of tickets to cancel for " << selectedMovie.name << ": ";
+    cin >> cancelTickets;
+    
+    if (cancelTickets < 0) {
+        cout << "You can't cancel a negative number of tickets!" << endl;
+    }
+    else if (cancelTickets > selectedMovie.bookedSeats) {
+        cout << "You can't cancel more tickets than you have booked!" << endl;
+    }
+    else {
+        selectedMovie.movieSeat += cancelTickets; 
+        selectedMovie.bookedSeats -= cancelTickets;
+        cout << "Tickets canceled! Now available seats for " << selectedMovie.name << " are: " << selectedMovie.movieSeat << endl;
+    }
 }
 int main()
 {
